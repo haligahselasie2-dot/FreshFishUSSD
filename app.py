fa21ce40350c1cd44dc643d85b2bdff96d75f25f
haligahselasie2-dot <haligahselasie2@gmail.com>
2025-11-22 11:53:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -25,3 +25,7 @@ if __name__ == "__main__":
     app = create_app()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+@app.route("/")
+def index():
+    return "TechHarvest system is live!"
